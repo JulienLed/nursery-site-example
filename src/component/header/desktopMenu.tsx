ea69ffea6ad8bdb510e5a7ff4bce55574f9211bc
Julien LEDENT <ledentj@gmail.com>
2025-09-26 14:05:44 +0200

    Quid content.map ?

--- src/component/header/desktopMenu.tsx
+++ src/component/header/desktopMenu.tsx
@@ -10,61 +10,123 @@ import {
 import { menu } from "../../data/data";
 import Link from "next/link";
 
-export default function DesktopMenu() {
+const fetchMenu = async () => {
+  const response = await fetch(
+    `${process.env.API_STRAPI}/api/menus?populate=*`,
+    {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${process.env.API_KEY_STRAPI}`,
+      },
+      cache: "no-store",
+    }
+  );
+  const jsonResponse = await response.json();
+  return jsonResponse.data;
+};
+
+export default async function DesktopMenu() {
+  const data = await fetchMenu();
   return (
     <NavigationMenu viewport={false} className="!font-fredoka">
       <NavigationMenuList className="flex justify-evenly gap-[3vw]">
-        {menu.map((item) => {
-          return (
-            <NavigationMenuItem key={item.title} className="!shadow-xl">
-              {item.content ? (
-                <>
-                  <NavigationMenuTrigger className="!transition-all !duration-200 hover:!text-chart-4">
-                    <NavigationMenuLink
-                      asChild
-                      className="hover:!bg-transparent"
-                    >
-                      <Link
-                        className="!text-lg hover:!text-chart-4"
-                        href={item.path}
-                      >
-                        {item.title}
-                      </Link>
-                    </NavigationMenuLink>
-                  </NavigationMenuTrigger>
-                  <NavigationMenuContent className="!w-60 !shadow-xl !border-0">
-                    {item.content.map((subItem) => (
+        {data.map(
+          (item: {
+            Titre: string;
+            Path: string;
+            ImageProject: {
+              height: number;
+              width: number;
+              url: string;
+            };
+            Member: {
+              Name: string;
+              Role: string;
+              ImageProject: {
+                height: number;
+                width: number;
+                url: string;
+              };
+              Description: string;
+              FunFact: string;
+            };
+            Projet: {
+              Titre: string;
+              Description: string;
+              Details: string[];
+              ImageProject: {
+                height: number;
+                width: number;
+                url: string;
+              };
+              Slug: string;
+            };
+            Service: {
+              Titre: string;
+              Slug: string;
+              Description: string;
+              ImageProject: {
+                height: number;
+                width: number;
+                url: string;
+              };
+            };
+            Tarif: {
+              jour: number;
+              price: number;
+            };
+          }) => {
+            return (
+              <NavigationMenuItem key={item.Titre} className="!shadow-xl">
+                {item.Projet && item.Service ? (
+                  <>
+                    <NavigationMenuTrigger className="!transition-all !duration-200 hover:!text-chart-4">
                       <NavigationMenuLink
-                        key={subItem.title}
                         asChild
-                        className="text-md"
+                        className="hover:!bg-transparent"
                       >
                         <Link
-                          href={subItem.path}
-                          className="hover:!text-chart-4"
+                          className="!text-lg hover:!text-chart-4"
+                          href={item.Path}
                         >
-                          {subItem.title}
+                          {item.Titre}
                         </Link>
                       </NavigationMenuLink>
-                    ))}
-                  </NavigationMenuContent>
-                </>
-              ) : (
-                <NavigationMenuLink
-                  asChild
-                  className={`${navigationMenuTriggerStyle()} !transition-all !duration-200`}
-                >
-                  <Link
-                    className="!text-lg hover:!text-chart-4"
-                    href={item.path}
+                    </NavigationMenuTrigger>
+                    <NavigationMenuContent className="!w-60 !shadow-xl !border-0">
+                      {item.content.map((subItem) => (
+                        <NavigationMenuLink
+                          key={subItem.title}
+                          asChild
+                          className="text-md"
+                        >
+                          <Link
+                            href={subItem.path}
+                            className="hover:!text-chart-4"
+                          >
+                            {subItem.title}
+                          </Link>
+                        </NavigationMenuLink>
+                      ))}
+                    </NavigationMenuContent>
+                  </>
+                ) : (
+                  <NavigationMenuLink
+                    asChild
+                    className={`${navigationMenuTriggerStyle()} !transition-all !duration-200`}
                   >
-                    {item.title}
-                  </Link>
-                </NavigationMenuLink>
-              )}
-            </NavigationMenuItem>
-          );
-        })}
+                    <Link
+                      className="!text-lg hover:!text-chart-4"
+                      href={item.Path}
+                    >
+                      {item.Titre}
+                    </Link>
+                  </NavigationMenuLink>
+                )}
+              </NavigationMenuItem>
+            );
+          }
+        )}
       </NavigationMenuList>
     </NavigationMenu>
   );
